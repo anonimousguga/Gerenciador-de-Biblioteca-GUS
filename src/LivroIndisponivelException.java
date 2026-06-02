1986027610e9e8275f617f316a6d8e54a85233ea
Gustavo <gustavo.batista4@pucpr.edu.br>
2026-06-01 22:31:17 -0300

    feat: implementacao da excecao LivroIndisponivelException

--- src/LivroIndisponivelException.java
+++ src/LivroIndisponivelException.java
@@ -1,1 +1,13 @@
+public class LivroIndisponivelException extends Exception {
 
+    private static final long serialVersionUID = 10L;
+    private String tituloLivro;
+    public LivroIndisponivelException(String tituloLivro) {
+        super("O livro \"" + tituloLivro + "\" não está disponível para empréstimo.");
+        this.tituloLivro = tituloLivro;
+    }
+
+    public String getTituloLivro() {
+        return tituloLivro;
+    }
+}
